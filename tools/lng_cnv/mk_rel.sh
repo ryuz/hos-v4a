44b9ff071a00019ea5194ce6cd6105ad01828112
ryuz <ryuz>
2006-10-15 02:46:17 +0000

    (none)

---
+++ tools/lng_cnv/mk_rel.sh
@@ -0,0 +1,37 @@
+#/usr/bin/sh
+
+# %jp{リリース用パッケージのようなものを生成}
+
+
+################################
+# Japanese                     #
+################################
+
+# unix
+rm -fr jp-euc-unix
+./dircpy.pl ../.. jp-euc-unix jp-euc-unix
+
+# windows
+rm -fr jp-sjis-dos
+./dircpy.pl ../.. jp-sjis-dos jp-sjis-dos
+
+# cygwin
+rm -fr jp-sjis-unix
+./dircpy.pl ../.. jp-sjis-unix jp-sjis-unix
+
+
+
+################################
+# English                      #
+################################
+
+# unix/cygwin
+rm -fr en-unix
+./dircpy.pl ../.. en-euc-unix jp-euc-unix
+
+# windows
+rm -fr en-dos
+./dircpy.pl ../.. en-euc-dos jp-euc-dos
+
+
+
