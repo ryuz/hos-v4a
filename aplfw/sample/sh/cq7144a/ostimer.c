a9b0d4404ec56f00439008a241ef7840cfbf25ea
ryuz <ryuz>
2006-11-14 15:55:54 +0000

    (none)

--- aplfw/sample/sh/cq7144a/ostimer.c
+++ aplfw/sample/sh/cq7144a/ostimer.c
@@ -11,69 +11,42 @@
 
 #include "kernel.h"
 #include "ostimer.h"
+#include "regs_sh7144.h"
 
 
-#define OSTIMER_TIMER_INHNO		24			/**< %jp{割り込みハンドラ番号} */
+#define INHNO_OSTIMER			144							/**< %jp{割り込みハンドラ番号} */
+#define PERIPHERAL_CLOCK		(24000000UL)				/**< %jp{ペリフェラルクロック(24MHz)} */
 
 
-#define REG_TIM16_TSTR		((volatile UB *)0xffffff60)
-#define REG_TIM16_TSNC		((volatile UB *)0xffffff61)
-#define REG_TIM16_TMDR		((volatile UB *)0xffffff62)
-#define REG_TIM16_TOLR		((volatile UB *)0xffffff63)
-#define REG_TIM16_TISRA		((volatile UB *)0xffffff64)
-#define REG_TIM16_TISRB		((volatile UB *)0xffffff65)
-#define REG_TIM16_TISRC		((volatile UB *)0xffffff66)
-
-#define REG_TIM16_TCR0		((volatile UB *)0xffffff68)
-#define REG_TIM16_TIOR0		((volatile UB *)0xffffff69)
-#define REG_TIM16_TCNT0		((volatile UH *)0xffffff6a)
-#define REG_TIM16_TCNT0H	((volatile UB *)0xffffff6a)
-#define REG_TIM16_TCNT0L	((volatile UB *)0xffffff6b)
-#define REG_TIM16_GRA0		((volatile UH *)0xffffff6c)
-#define REG_TIM16_GRA0H		((volatile UB *)0xffffff6c)
-#define REG_TIM16_GRA0L		((volatile UB *)0xffffff6d)
-#define REG_TIM16_GRB0		((volatile UH *)0xffffff6e)
-#define REG_TIM16_GRB0H		((volatile UB *)0xffffff6e)
-#define REG_TIM16_GRB0L		((volatile UB *)0xffffff6f)
-
-
-static void OsTimer_IrqHandler(VP_INT exinf);		/**< %jp{タイマ割り込みハンドラ} */
+static void OsTimer_IrqHandler(void);						/**< %jp{タイマ割り込みハンドラ} */
 
 
 /** %jp{OS用タイマ初期化ルーチン} */
 void OsTimer_Initialize(VP_INT exinf)
 {
-#if 0
 	T_DINH dfinh;
 	
 	/* %jp{割り込みハンドラ定義} */
 	dfinh.inthdr = (FP)OsTimer_IrqHandler;
-	def_inh(OSTIMER_TIMER_INHNO, &dfinh);
-#else
-	T_CISR cisr;
-	cisr.isratr = TA_HLNG;
-	cisr.intno  = OSTIMER_TIMER_INHNO;
-	cisr.isr    = (FP)OsTimer_IrqHandler;
-	cisr.exinf  = (VP_INT)'@';
-	acre_isr(&cisr);
-#endif
-
+	def_inh(INHNO_OSTIMER, &dfinh);
+	
 	/* %jp{タイマ動作開始} */
-	*REG_TIM16_TSTR  &= ~0x01;		/* TCNT0動作停止 */
-	*REG_TIM16_TCR0   = 0x23;		/* φ/8でカウント, GRAのコンペアマッチでクリア */
-	*REG_TIM16_TCNT0  = 0;			/* カウンタクリア */
-	*REG_TIM16_GRA0   = 2500 - 1;	/* 10 msec のインターバル（20MHz/8 = 2.5MHz） */
-	*REG_TIM16_TISRA &= ~0x01;		/* 割り込み要因クリア */
-	*REG_TIM16_TISRA |= 0x10;		/* 割り込み許可 */
-	*REG_TIM16_TSTR  |= 0x01;		/* TCNT0動作開始 */
+	*REG_STANDBY_MSTCR2 &= 0xefff;
+	*REG_CMT_CMSTR  &= 0xfffe;								/* %jp{動作停止} */
+	*REG_CMT0_CMCNT  = 0x0000;								/* %jp{カウンタクリア} */
+	*REG_CMT0_CMCOR  = (PERIPHERAL_CLOCK / 128) / 1000;		/* %jp{1msに設定} */
+	*REG_CMT0_CMCSR  = 0x0042;								/* %jp{128分周に設定} */
+	*REG_CMT_CMSTR  |= 0x0001;								/* %jp{動作開始} */
+	
+	/* %jp{割り込み許可} */
+	*REG_INTC_IPRG = ((*REG_INTC_IPRG & 0xff0f) | 0x0010);
 }
 
 
 /** %jp{タイマ割り込みハンドラ} */
-void OsTimer_IrqHandler(VP_INT exinf)
+void OsTimer_IrqHandler(void)
 {
-	/* 割り込み要因クリア */
-	*REG_TIM16_TISRA &= ~0x01;		/* 割り込み要因クリア */
+	*REG_CMT0_CMCSR &= 0xff7f;
 	
 	isig_tim();
 }
