45e1e64dd253486ca56e6346bbaaaf2425d01a2e
ryuz <ryuz>
2006-11-16 12:38:16 +0000

    (none)

---
+++ aplfw/hosaplfw.h
@@ -0,0 +1,16 @@
+
+/* ユーザープロセスから使っても良いAPIを一括読み込み */
+
+#ifndef __HOS__hosaplfw_h__
+#define __HOS__hosaplfw_h__
+
+#include "system/handle/handle.h"
+#include "system/memory/memory.h"
+#include "system/process/process.h"
+#include "system/command/command.h"
+#include "system/file/file.h"
+#include "system/file/stdfile.h"
+
+
+#endif __HOS__hosaplfw_h__
+
