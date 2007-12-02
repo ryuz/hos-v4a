c6fd376a257f4e10e99f7613ae5df0a38e1eb46d
ryuz <ryuz>
2007-12-02 12:32:27 +0000

    (none)

---
+++ kernel/source/object/mtx/kadd_mtx.c
@@ -0,0 +1,54 @@
+/** 
+ *  Hyper Operating System V4 Advance
+ *
+ * @file  kadd_mtx.c
+ * @brief %jp{ミューテックスをTCBに接続}%en{}
+ *
+ * Copyright (C) 1998-2006 by Project HOS
+ * http://sourceforge.jp/projects/hos/
+ */
+
+
+
+#include "core/core.h"
+#include "object/mtxobj.h"
+
+
+/* %jp{ミューテックスをTCBに接続} */
+void _kernel_add_mtx(_KERNEL_T_MTXCB_PTR mtxcb, _KERNEL_T_MTXHDL mtxhdl, _KERNEL_T_TCB_PTR tcb)
+{
+	_KERNEL_T_MTXHDL		mtxhdl_head;
+	
+	mtxhdl = _KERNEL_MTX_GET_MTXHDL(mtxid, mtxcb);
+			
+	mtxhdl_head = _KERNEL_TSK_GET_MTXHDL(tcb);
+	if ( mtxhdl_head == _KERNEL_MTXHDL_NULL )
+	{
+		/* %jp{最初の１つを接続} */
+		_KERNEL_TSK_SET_MTXHDL(tcb, mtxhdl);
+		_KERNEL_MTX_SET_NEXT(mtxcb, mtxhdl);
+		_KERNEL_MTX_SET_NEXT(mtxcb, mtxhdl);
+	}
+	else
+	{
+		_KERNEL_T_MTXHDL	mtxhdl_next;
+		_KERNEL_T_MTXHDL	mtxhdl_prev;
+		_KERNEL_T_MTXCB_PTR	mtxcb_next;
+		_KERNEL_T_MTXCB_PTR	mtxcb_prev;
+
+		/* %jp{接続の先頭/末尾を取得} */
+		mtxhdl_next = mtxhdl_head;
+		mtxcb_next  = _KERNEL_MTX_MTXHDL2MTXCB(mtxhdl_next);
+		mtxhdl_prev = _KERNEL_MTX_GET_PREV(mtxcb_next);
+		mtxcb_prev  = _KERNEL_MTX_MTXHDL2MTXCB(mtxhdl_prev);
+
+		/* %jp{末尾に接続} */
+		_KERNEL_MTX_SET_PREV(mtxcb_next, mtxhdl);
+		_KERNEL_MTX_SET_NEXT(mtxcb_prev, mtxhdl);
+		_KERNEL_MTX_SET_NEXT(mtxcb, mtxhdl_next);
+		_KERNEL_MTX_SET_PREV(mtxcb, mtxhdl_prev);
+	}
+}
+
+
+/* end of file */
---
+++ kernel/source/object/mtx/krmv_mtx.c
@@ -0,0 +1,72 @@
+/** 
+ *  Hyper Operating System V4 Advance
+ *
+ * @file  kadd_mtx.c
+ * @brief %jp{ミューテックスをTCBに接続}%en{}
+ *
+ * Copyright (C) 1998-2006 by Project HOS
+ * http://sourceforge.jp/projects/hos/
+ */
+
+
+
+#include "core/core.h"
+#include "object/mtxobj.h"
+
+
+/* %jp{ミューテックスをTCBから取り外し} */
+void _kernel_rmv_mtx(_KERNEL_T_MTXCB_PTR mtxcb, _KERNEL_T_MTXHDL mtxhdl, _KERNEL_T_TCB_PTR tcb)
+{
+	_KERNEL_T_MTXHDL	mtxhdl_head;
+	_KERNEL_T_MTXHDL	mtxhdl_next;
+	_KERNEL_T_MTXHDL	mtxhdl_prev;
+	_KERNEL_T_MTXCB_PTR	mtxcb_next;
+	_KERNEL_T_MTXCB_PTR	mtxcb_prev;
+	_KERNEL_T_TSKHDL	tskhdl;
+
+	
+	/* 取り外す */
+	mtxhdl_next = _KERNEL_MTX_GET_NEXT(mtxcb);
+	if ( mtxhdl_next == mtxhdl )
+	{
+		/* %jp{最後の１つを取り外し} */
+		_KERNEL_TSK_SET_MTXHDL(tcb, _KERNEL_MTXHDL_NULL);
+	}
+	else
+	{
+		/* %jp{前後の接続を取得} */
+		mtxcb_next  = _KERNEL_MTX_MTXHDL2MTXCB(mtxhdl_next);
+		mtxhdl_prev = _KERNEL_MTX_GET_PREV(mtxcb_next);
+		mtxcb_prev  = _KERNEL_MTX_MTXHDL2MTXCB(mtxhdl_prev);
+		
+		/* %jp{接続解除} */
+		_KERNEL_MTX_SET_PREV(mtxcb_next, mtxhdl_prev);
+		_KERNEL_MTX_SET_NEXT(mtxcb_prev, mtxhdl_next);
+		
+		/* %jp{先頭ならずらす} */
+		mtxhdl_head = _KERNEL_TSK_GET_MTXHDL(tcb);
+		if ( mtxhdl_head == mtxhdl )
+		{
+			_KERNEL_TSK_SET_MTXHDL(tcb, mtxhdl_next);
+		}
+	}
+	
+	/* %jp{待ちタスクがあれば起す} */
+	tskhdl = _KERNEL_MTX_RMH_QUE(mtxcb);
+	if ( tskhdl != _KERNEL_TSKHDL_NULL )
+	{
+		/* %jp{TCB取得} */
+		tcb = _KERNEL_TSK_TSKHDL2TCB(tskhdl);
+		
+		/* ミューテックを繋ぐ */
+		_kernel_add_mtx(mtxcb, mtxhdl, tcb);
+		
+		/* %jp{タスク待ち解除} */
+		_KERNEL_TSK_SET_ERCD(tcb, E_OK);			/* %jp{エラーコード設定} */
+		_KERNEL_DSP_WUP_TSK(tskhdl);				/* %jp{タスクの待ち解除} */
+		_KERNEL_MTX_RMV_TOQ(tskhdl);
+	}
+}
+
+
+/* end of file */
