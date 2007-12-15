a9447301ddba5a0ca952e9d8de70f429901cf3a1
ryuz <ryuz>
2007-12-15 03:38:41 +0000

    (none)

---
+++ kernel/source/object/tsk/kchg_pri.c
@@ -0,0 +1,38 @@
+/** 
+ *  Hyper Operating System V4 Advance
+ *
+ * @file  kchg_pri.c
+ * @brief %jp{タスク優先度の変更}
+ *
+ * Copyright (C) 1998-2007 by Project HOS
+ * http://sourceforge.jp/projects/hos/
+ */
+
+
+#include "core/core.h"
+
+
+/** %jp{タスク優先度の変更} */
+ER _kernel_chg_pri(_KERNEL_T_TSKHDL tskhdl, PRI tskpri)
+{
+	_KERNEL_T_TCB_PTR  tcb;
+	
+	/* %jp{タスクハンドルを取得} */
+	tcb = _KERNEL_TSK_TSKHDL2TCB(tskhdl);
+	
+	/* %jp{新しいタスク優先度を設定} */
+	_KERNEL_TSK_SET_TSKPRI(tcb, tskpri);
+	
+	
+	/* %jp{レディーキューを繋ぎ直す} */
+	if ( _KERNEL_TSK_SNS_RDY(tcb) )
+	{
+		_KERNEL_SYS_RMV_RDQ(tskhdl);
+		_KERNEL_SYS_ADD_RDQ(tskhdl);
+	}
+	
+	return E_OK;
+}
+
+
+/* end of file */
