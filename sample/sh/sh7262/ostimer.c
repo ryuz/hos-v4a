ac3cf4c60bd99c9c62ce49a60c8165c07e43c348
ryuz <ryuz@users.sourceforge.jp>
2010-11-21 23:51:44 +0900

    add sh7262

--- sample/sh/sh7262/ostimer.c
+++ sample/sh/sh7262/ostimer.c
@@ -4,7 +4,7 @@
  * @file  ostimer.c
  * @brief %jp{OSタイマ}%en{OS timer}
  *
- * Copyright (C) 1998-2006 by Project HOS
+ * Copyright (C) 1998-2010 by Project HOS
  * http://sourceforge.jp/projects/hos/
  */
 
@@ -14,6 +14,8 @@
 #include "regs_sh7262.h"
 
 
+#define INHNO_OSTIMER			175							/**< %jp{割込みハンドラ番号} */
+#define PERIPHERAL_CLOCK		(24000000UL)				/**< %jp{ペリフェラルクロック(24MHz)} */
 
 
 static void OsTimer_IrqHandler(void);						/**< %jp{タイマ割込みハンドラ} */
@@ -26,17 +28,26 @@ void OsTimer_Initialize(VP_INT exinf)
 	
 	/* %jp{割込みハンドラ定義} */
 	dfinh.inthdr = (FP)OsTimer_IrqHandler;
-/*	def_inh(INHNO_OSTIMER, &dfinh);	*/
+	def_inh(INHNO_OSTIMER, &dfinh);
 	
 	/* %jp{タイマ動作開始} */
+	*REG_STBCR7     &= 0xfffb;								/* %jp{クロック供給} */
+	*REG_CMT_CMSTR  &= 0xfffe;								/* %jp{動作停止} */
+	*REG_CMT0_CMCNT  = 0x0000;								/* %jp{カウンタクリア} */
+	*REG_CMT0_CMCOR  = (PERIPHERAL_CLOCK / 128) / 1000;		/* %jp{1msに設定} */
+	*REG_CMT0_CMCSR  = 0x0042;								/* %jp{128分周に設定} */
+	*REG_CMT_CMSTR  |= 0x0001;								/* %jp{動作開始} */
 	
 	/* %jp{割込み許可} */
+	*REG_INTC_IPR10 = ((*REG_INTC_IPR10 & 0xff0f) | 0x0010);
 }
 
 
 /** %jp{タイマ割込みハンドラ} */
 void OsTimer_IrqHandler(void)
 {
+	/* %jp{フラグクリア}%en{flag clear} */
+	*REG_CMT0_CMCSR &= 0xff7f;
 	
 	isig_tim();
 }
--- sample/sh/sh7262/regs_sh7262.h
+++ sample/sh/sh7262/regs_sh7262.h
@@ -6,14 +6,26 @@
 #define __sh7262_regs_h__
 
 
-/* SCI0 */
-#define	REG_SCI0_SMR		((volatile UH*)0xffff8000)		/* シリアルモードレジスタ_0 */
-#define	REG_SCI0_BRR		((volatile UB*)0xffff8004)		/* ビットレートレジスタ_0 */
-#define	REG_SCI0_SCR		((volatile UH*)0xffff8008)		/* シリアルコントロールレジスタ_0 */
-#define	REG_SCI0_FTDR		((volatile UB*)0xffff800c)		/* 送信FIFOデータレジスタ_0 */
-#define	REG_SCI0_SSR		((volatile UH*)0xffff8010)		/* シリアルステータスレジスタ_0 */
-#define	REG_SCI0_FRDR		((volatile UB*)0xffff8014)		/* 受信FIFOデータレジスタ_0 */
+#define REG_INTC_IPR10		((volatile UH *)0xfffe0C08)
 
+#define	REG_STBCR7			((volatile UB *)0xfffe0418)
+
+/* SCI0 */
+#define	REG_SCI0_SMR		((volatile UH *)0xffff8000)		/* シリアルモードレジスタ_0 */
+#define	REG_SCI0_BRR		((volatile UB *)0xffff8004)		/* ビットレートレジスタ_0 */
+#define	REG_SCI0_SCR		((volatile UH *)0xffff8008)		/* シリアルコントロールレジスタ_0 */
+#define	REG_SCI0_FTDR		((volatile UB *)0xffff800c)		/* 送信FIFOデータレジスタ_0 */
+#define	REG_SCI0_SSR		((volatile UH *)0xffff8010)		/* シリアルステータスレジスタ_0 */
+#define	REG_SCI0_FRDR		((volatile UB *)0xffff8014)		/* 受信FIFOデータレジスタ_0 */
+
+/* CMT */
+#define REG_CMT_CMSTR		((volatile UH *)0xfffec000)		/* コンペアマッチタイマコントロールレジスタ */
+#define REG_CMT0_CMCSR		((volatile UH *)0xfffec002)		/* コンペアマッチタイマコントロール／ステータスレジスタ_0 */
+#define REG_CMT0_CMCNT		((volatile UH *)0xfffec004)		/* コンペアマッチカウンタ_0 */
+#define REG_CMT0_CMCOR		((volatile UH *)0xfffec006)		/* コンペアマッチコンスタントレジスタ_0 */
+#define REG_CMT1_CMCSR		((volatile UH *)0xfffec008)		/* コンペアマッチタイマコントロール／ステータスレジスタ_1 */
+#define REG_CMT1_CMCNT		((volatile UH *)0xfffec00a)		/* コンペアマッチカウンタ_1 */
+#define REG_CMT1_CMCOR		((volatile UH *)0xfffec00c)		/* コンペアマッチコンスタントレジスタ_1 */
 
 
 #endif	/* __sh7262_regs_h__ */
