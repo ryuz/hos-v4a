a5bded78f458324c76f82d36dcc0d2c262371830
ryuz <ryuz@users.sourceforge.jp>
2011-04-06 00:13:30 +0900

    modify linker scripts

--- kernel/include/arch/irc/mb/axi_intc/ircatr.h
+++ kernel/include/arch/irc/mb/axi_intc/ircatr.h
@@ -17,7 +17,7 @@
 
 #define _KERNEL_IRCATR_IRC					TRUE		/**< %jp{IRCの有無} */
 #define _KERNEL_IRCATR_REG_BASE				TRUE		/**< %jp{IRCレジスタベースアドレスの有無} */
-#define _KERNEL_IRCATR_REG_BASE_DEF			0x00000000	/**< %jp{デフォルトのIRCレジスタベースアドレス} */
+#define _KERNEL_IRCATR_REG_BASE_DEF			0x41200000	/**< %jp{デフォルトのIRCレジスタベースアドレス} */
 
 #define _KERNEL_IRCATR_TMIN_INTNO			0			/**< %jp{割込み番号の最小値} */
 #define _KERNEL_IRCATR_TMAX_INTNO			31			/**< %jp{割込み番号の最大値} */
