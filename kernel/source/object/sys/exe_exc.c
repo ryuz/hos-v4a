334b480e8ef556c2c4c3a4aab190d4ccd5c25955
ryuz <ryuz@users.sourceforge.jp>
2011-04-05 23:45:30 +0900

    modify MicroBlaze

---
+++ kernel/source/object/sys/exe_exc.c
@@ -0,0 +1,21 @@
+/**
+ *  Hyper Operating System V4 Advance
+ *
+ * @file  exe_exc.c
+ * @brief %jp{例外ハンドラの処理}
+ *
+ * Copyright (C) 1998-2011 by Project HOS
+ * http://sourceforge.jp/projects/hos/
+ */
+
+
+#include "core/core.h"
+
+
+/* %jp{例外ハンドラの処理} */
+void _kernel_exe_exc(EXCNO inhno)
+{
+}
+
+
+/* end of file */
