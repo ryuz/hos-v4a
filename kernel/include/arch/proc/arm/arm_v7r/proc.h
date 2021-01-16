3f85412df1afa22d926d4ed329f52b6cd169f206
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-01-16 21:07:03 +0900

    add mpu control

--- kernel/include/arch/proc/arm/arm_v7r/proc.h
+++ kernel/include/arch/proc/arm/arm_v7r/proc.h
@@ -64,6 +64,26 @@ void    _kernel_abt_hdr(void);
 void    _kernel_irq_hdr(void);
 void    _kernel_fiq_hdr(void);
 
+/* MPU Type Register */
+UW      _kernel_ref_cp15c0c0_4(void);
+
+/* MPU Region Size and Enable Registers */
+void    _kernel_chg_cp15c6c1_0(UW v);
+UW      _kernel_ref_cp15c6c1_0(void);
+
+/* c6, MPU Region Number Register */
+void    _kernel_chg_cp15c6c2_0(UW v);
+UW      _kernel_ref_cp15c6c2_0(void);
+
+/* MPU Region Access Control Register */
+void    _kernel_chg_cp15c6c1_4(UW v);
+UW      _kernel_ref_cp15c6c1_4(void);
+
+/* Data MPU Region Size and Enable Register */
+void    _kernel_chg_cp15c6c1_2(UW v);
+UW      _kernel_ref_cp15c6c1_2(void);
+
+
 #ifdef __cplusplus
 }
 #endif
@@ -86,6 +106,61 @@ void    _kernel_fiq_hdr(void);
 #define _KERNEL_SWI_CTX(pk_ctxcb_now, pk_ctxinf_nxt)							\
 									_kernel_swi_ctx((pk_ctxinf_nxt), (pk_ctxcb_now))				/**< %jp{実行コンテキストの切替} */
 
+
+#define _KERNEL_MPU_DISABLE					(0)
+#define _KERNEL_MPU_SIZE_32					((0x04 << 1) | 1)
+#define _KERNEL_MPU_SIZE_64					((0x05 << 1) | 1)
+#define _KERNEL_MPU_SIZE_128				((0x06 << 1) | 1)
+#define _KERNEL_MPU_SIZE_256				((0x07 << 1) | 1)
+#define _KERNEL_MPU_SIZE_512				((0x08 << 1) | 1)
+#define _KERNEL_MPU_SIZE_1K					((0x09 << 1) | 1)
+#define _KERNEL_MPU_SIZE_2K					((0x0a << 1) | 1)
+#define _KERNEL_MPU_SIZE_4K					((0x0b << 1) | 1)
+#define _KERNEL_MPU_SIZE_8K					((0x0c << 1) | 1)
+#define _KERNEL_MPU_SIZE_16K				((0x0d << 1) | 1)
+#define _KERNEL_MPU_SIZE_32K				((0x0e << 1) | 1)
+#define _KERNEL_MPU_SIZE_64K				((0x0f << 1) | 1)
+#define _KERNEL_MPU_SIZE_128K				((0x10 << 1) | 1)
+#define _KERNEL_MPU_SIZE_256K				((0x11 << 1) | 1)
+#define _KERNEL_MPU_SIZE_512K				((0x12 << 1) | 1)
+#define _KERNEL_MPU_SIZE_1M					((0x13 << 1) | 1)
+#define _KERNEL_MPU_SIZE_2M					((0x14 << 1) | 1)
+#define _KERNEL_MPU_SIZE_4M					((0x15 << 1) | 1)
+#define _KERNEL_MPU_SIZE_8M					((0x16 << 1) | 1)
+#define _KERNEL_MPU_SIZE_16M				((0x17 << 1) | 1)
+#define _KERNEL_MPU_SIZE_32M				((0x18 << 1) | 1)
+#define _KERNEL_MPU_SIZE_64M				((0x19 << 1) | 1)
+#define _KERNEL_MPU_SIZE_128M				((0x1a << 1) | 1)
+#define _KERNEL_MPU_SIZE_256M				((0x1b << 1) | 1)
+#define _KERNEL_MPU_SIZE_512M				((0x1c << 1) | 1)
+#define _KERNEL_MPU_SIZE_1G					((0x1d << 1) | 1)
+#define _KERNEL_MPU_SIZE_2G					((0x1e << 1) | 1)
+#define _KERNEL_MPU_SIZE_4G					((0x1f << 1) | 1)
+
+#define _KERNEL_MPU_XN						(1 << 12)
+#define _KERNEL_MPU_S						(1 << 2)
+
+#define _KERNEL_MPU_AP_NO					(0x0 << 8)
+#define _KERNEL_MPU_AP_FULL					(0x3 << 8)
+
+#define _KERNEL_MPU_STRONGLY_ORDERED		((0x0 << 3) | 0x0)
+#define _KERNEL_MPU_SHAREABLE_DEVICE		((0x0 << 3) | 0x1)
+#define _KERNEL_MPU_WRITE_THROUGH			((0x0 << 3) | 0x2)
+#define _KERNEL_MPU_WRITE_BACK				((0x0 << 3) | 0x3)
+#define _KERNEL_MPU_NO_CACHEABLE			((0x1 << 3) | 0x0)
+#define _KERNEL_MPU_WRITE_BACK_ALLOC		((0x1 << 3) | 0x3)
+#define _KERNEL_MPU_NON_SHAREABLE_DEVICE	((0x2 << 3) | 0x0)
+
+#define _KERNEL_MPU_2_NO_CACHEABLE			((0x4 << 3) | 0x0)
+#define _KERNEL_MPU_2_WRITE_BACK_ALLOC		((0x4 << 3) | 0x1)
+#define _KERNEL_MPU_2_WRITE_THROUGH			((0x4 << 3) | 0x2)
+#define _KERNEL_MPU_2_WRITE_BACK			((0x4 << 3) | 0x3)
+
+#define vmpu_get_number_of_regions()		((_kernel_ref_cp15c0c0_4() >> 8) & 0xff)
+#define vmpu_set_region_number(v)			do { _kernel_chg_cp15c6c2_0(v); } while(0)
+#define vmpu_set_region_base_address(v)		do { _kernel_chg_cp15c6c1_0(v); } while(0)
+#define vmpu_set_region_size(v)				do { _kernel_chg_cp15c6c1_2(v); } while(0)
+#define vmpu_set_region_access_control(v)   do { _kernel_chg_cp15c6c1_4(v); } while(0)
 
 
 #endif	/* _KERNEL__arch__proc__arm__arm_v7r__proc_h__ */
