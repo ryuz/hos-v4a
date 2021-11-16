26b627a07091da6c89887b424c4780fade8ab5b6
Takeharu KATO <tkato@elliptic-curve.net>
2021-11-17 00:16:28 +0900

    修正概要: RISC-Vのコンテキスト情報中のレジスタオフセットを定義するヘッダファイルを追加           (自動生成ファイル)。           Add a header file to define register offsets in contexts for RISC-V           (automatically generated file).
       kernel/include/arch/proc/riscv/regoff.h
         RISC-Vのコンテキスト中のレジスタオフセットを定義したオフセットファイルです。
    
       kernel/include/arch/proc/riscv/regoff.h
         The offset-file contains register offsets in contexts for RISC-V.
         Note that this is an automatically generated file by the offset file generator.

---
+++ kernel/include/arch/proc/riscv/regoff.h
@@ -0,0 +1,55 @@
+/**
+ *  Hyper Operating System V4 Advance
+ *
+ * @file regoff.h
+ * @brief Structure offset definitions
+ * @note  This file is generated automatically, don't edit by hand.
+ *
+ * Copyright (C) 1998-2008 by Project HOS
+ * http://sourceforge.jp/projects/hos/
+ */
+
+
+#if !defined(_KERNEL__kernel__include__arch__proc__riscv__regoff_h__)
+#define _KERNEL__kernel__include__arch__proc__riscv__regoff_h__
+
+#define _KERNEL_RISCV_PROC_ICTX_SIZE (144) /*< sizeof(struct _kernel_riscv_ictx) */
+#define _KERNEL_RISCV_PROC_ICTX_RA (0) /*< __asm_offsetof(struct _kernel_riscv_ictx, ra) */
+#define _KERNEL_RISCV_PROC_ICTX_T0 (8) /*< __asm_offsetof(struct _kernel_riscv_ictx, t0) */
+#define _KERNEL_RISCV_PROC_ICTX_T1 (16) /*< __asm_offsetof(struct _kernel_riscv_ictx, t1) */
+#define _KERNEL_RISCV_PROC_ICTX_T2 (24) /*< __asm_offsetof(struct _kernel_riscv_ictx, t2) */
+#define _KERNEL_RISCV_PROC_ICTX_T3 (32) /*< __asm_offsetof(struct _kernel_riscv_ictx, t3) */
+#define _KERNEL_RISCV_PROC_ICTX_T4 (40) /*< __asm_offsetof(struct _kernel_riscv_ictx, t4) */
+#define _KERNEL_RISCV_PROC_ICTX_T5 (48) /*< __asm_offsetof(struct _kernel_riscv_ictx, t5) */
+#define _KERNEL_RISCV_PROC_ICTX_T6 (56) /*< __asm_offsetof(struct _kernel_riscv_ictx, t6) */
+#define _KERNEL_RISCV_PROC_ICTX_A0 (64) /*< __asm_offsetof(struct _kernel_riscv_ictx, a0) */
+#define _KERNEL_RISCV_PROC_ICTX_A1 (72) /*< __asm_offsetof(struct _kernel_riscv_ictx, a1) */
+#define _KERNEL_RISCV_PROC_ICTX_A2 (80) /*< __asm_offsetof(struct _kernel_riscv_ictx, a2) */
+#define _KERNEL_RISCV_PROC_ICTX_A3 (88) /*< __asm_offsetof(struct _kernel_riscv_ictx, a3) */
+#define _KERNEL_RISCV_PROC_ICTX_A4 (96) /*< __asm_offsetof(struct _kernel_riscv_ictx, a4) */
+#define _KERNEL_RISCV_PROC_ICTX_A5 (104) /*< __asm_offsetof(struct _kernel_riscv_ictx, a5) */
+#define _KERNEL_RISCV_PROC_ICTX_A6 (112) /*< __asm_offsetof(struct _kernel_riscv_ictx, a6) */
+#define _KERNEL_RISCV_PROC_ICTX_A7 (120) /*< __asm_offsetof(struct _kernel_riscv_ictx, a7) */
+#define _KERNEL_RISCV_PROC_ICTX_EPC (128) /*< __asm_offsetof(struct _kernel_riscv_ictx, epc) */
+#define _KERNEL_RISCV_PROC_ICTX_STATUS (136) /*< __asm_offsetof(struct _kernel_riscv_ictx, status) */
+#define _KERNEL_RISCV_PROC_TSKCTX_SIZE (112) /*< sizeof(struct _kernel_riscv_tskswctx) */
+#define _KERNEL_RISCV_PROC_TSKCTX_RA (0) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, ra) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S0 (8) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s0) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S1 (16) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s1) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S2 (24) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s2) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S3 (32) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s3) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S4 (40) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s4) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S5 (48) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s5) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S6 (56) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s6) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S7 (64) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s7) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S8 (72) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s8) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S9 (80) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s9) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S10 (88) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s10) */
+#define _KERNEL_RISCV_PROC_TSKCTX_S11 (96) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, s11) */
+#define _KERNEL_RISCV_PROC_TSKCTX_TP (104) /*< __asm_offsetof(struct _kernel_riscv_tskswctx, tp) */
+
+
+#endif  /*  _KERNEL__kernel__include__arch__proc__riscv__regoff_h__  */
+
+
+/* end of file */
